363cc16f13bda8c4f1b7a3d2b469c048d04b5c68
Armaan Mohar <armaan.mohar@gmail.com>
2020-10-08 21:04:38 -0600

    posts

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import SignUp from './components/auth/SignUp.jsx';
 import AdminHome from './components/admin/AdminHome.jsx';
 import LogIn from './components/auth/LogIn.jsx';
+import Post from './components/posts/Post.jsx';
+import CreatePost from './components/posts/CreatePost.jsx';
+import PostList from './components/posts/PostList.jsx';
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
           <Route exact path='/' component={SignUp}/>
           <Route path='/admin' component={AdminHome}/>
           <Route exact path='/login' component={LogIn}/>
+          <Route exact path='/posts' component={Post}/>
+          <Route exact path='/createpost' component={CreatePost}/>
+          <Route exact path='/postlist' component={PostList}/>
         </Switch>
       </BrowserRouter>
 
---
+++ client/src/components/posts/Post.jsx
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import './Post.css';
+
+
+class Post extends Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        const JS = 0;
+        return(
+            <div className="post-body">
+                <table className="post-table">
+                    <tr><th>Course Number </th>
+                        <th>Experience</th>
+                        <th>Course Taken in</th>
+                        <th>Date Posted:</th> 
+                        </tr>
+                    <tr>    <td> MATH 72613</td>   {/*course id*/}
+                            <td>blah blah blah blah blah blah blah</td>   {/*content of post*/}
+                            <td> April 2019</td>   {/*year course was taken by user*/}
+                            <td> today </td>   {/*date post created*/}
+                             </tr>
+                    <tr>    <th>Stars:</th>
+                            <th>Likes: </th>
+                            <th>Dislikes</th>
+                             </tr>
+                    <tr>
+                        <td>{JS}</td>   {/*number of stars*/}
+                        <td> <button>Like</button> {JS} </td>   {/*like btn & current # of Likes*/}
+                        <td> <button>Dislike</button> {JS} </td>    {/*dislike btn & current # of dislikes*/}
+                    </tr>
+                </table>
+            </div>
+        )
+        
+    }
+}
+
+export default Post;
