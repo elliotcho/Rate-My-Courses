d8997b804c00686ea3d5aff60cb95b73d1fb18bc
Yong <ycho97@uwo.ca>
2020-10-13 17:16:31 -0400

    more auth model frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import Signup from './components/auth/Signup.jsx';
 import AdminHome from './components/admin/AdminHome.jsx';
-import Login from './components/auth/Login.jsx';
 import Post from './components/posts/Post.jsx';
 import CreatePost from './components/posts/CreatePost.jsx';
 import PostList from './components/posts/PostList.jsx';
 import Departments from './components/admin/Departments.jsx'
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import AuthModal from './components/auth/AuthModal';
 import Navigate from './components/Layout/Navigate.jsx';
-import AuthModal from './components/auth/AuthModal.jsx';
 function App() {
   
   // <Navigate></Navigate>
@@ -16,13 +14,23 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path='/auth' component={AuthModal}/>
           <Route path='/admin' component={AdminHome}/>
-          <Route exact path='/login' component={Login}/>
           <Route exact path='/posts' component={Post}/>
           <Route exact path='/createpost' component={CreatePost}/>
           <Route exact path='/postlist' component={PostList}/>
-          <Route exact path='/Departments' component={Departments}/>
+          <Route exact path='/departments' component={Departments}/>
+          <Route exact path='/auth' render = {() =>
+            <div>
+
+                <div>
+                    <button data-toggle='modal' data-target='#auth'>
+                      Sign In
+                    </button>
+
+                    <AuthModal/>
+                </div>
+            </div>
+          }/>
         </Switch>
       </BrowserRouter>
 
--- client/src/components/auth/AuthModal.jsx
+++ client/src/components/auth/AuthModal.jsx
@@ -1,56 +1,44 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Login from './Login';
 import Signup from './Signup';
 import './css/AuthModal.css';
 
-class AuthModal extends Component{
-    render(){
-        return(
-            <div>
-                <button data-toggle='modal' data-target='#auth'>
-                    Sign In
-                </button>
-
-                <div className='auth-modal modal fade' id='auth' data-backdrop='static'>
-                    <div className ='modal-dialog modal-dialog-centered'>
-                        <div className ='modal-content'>
-                            <div className ='modal-header'>
-                                <ul className='nav nav-tabs'>
-                                    <li>
-                                        <a className='nav-link active' data-toggle='tab' href='#login'>
-                                            Login
-                                        </a>
-                                    </li>
-
-                                    <li>
-                                        <a className='nav-link' data-toggle='tab' href='#signup'>
-                                            Sign Up
-                                        </a>
-                                    </li>
-                                </ul>
+function AuthModal(){
+    return(
+        <div className='auth-modal modal fade' id='auth' data-backdrop='static'>
+            <div className ='modal-dialog modal-dialog-centered'>
+                <div className ='modal-content'>
+                    <div className ='modal-header'>
+                        <div className='nav nav-tabs'>                                 
+                            <a className='active' data-toggle='tab' href='#login'>
+                                Login
+                            </a>
+                                    
+                            <a data-toggle='tab' href='#signup'>
+                                Sign Up
+                            </a>
+                        </div>
                     
-                                <button id='close-auth' className='close' data-dismiss='modal'>
-                                    <span>&times;</span>
-                                </button>
-                            </div>
+                        <button id='close-auth' className='close' data-dismiss='modal'>
+                            <span>&times;</span>
+                        </button>
+                    </div>
 
-                            <div className ='modal-body'>
-                                <div className='tab-content'>
-                                    <section id='login' className ='tab-pane active'>
-                                        <Login/>
-                                    </section>
+                    <div className ='modal-body'>
+                        <div className='tab-content'>
+                            <section id='login' className ='tab-pane active'>
+                                <Login/>
+                            </section>
 
-                                    <section id='signup' className ='tab-pane'>
-                                        <Signup/>
-                                    </section>
-                                </div>
-                            </div>
+                            <section id='signup' className ='tab-pane'>
+                                <Signup/>
+                            </section>
                         </div>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default AuthModal;
