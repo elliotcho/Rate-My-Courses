1abae5322c1974477d1c6022a1bf755e9ff04fb5
Gugsa Challa <gchal@Gugsas-MacBook-Pro.local>
2020-10-12 22:19:47 -0400

    pushing User Page

--- client/src/components/admin/Departments.jsx
+++ client/src/components/admin/Departments.jsx
@@ -4,7 +4,7 @@ class Departments extends Component{
     render(){
         return(
             <div>
-                <h1>Departments</h1>
+                
             </div>
         )
     }
--- client/src/components/admin/Users.jsx
+++ client/src/components/admin/Users.jsx
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
-
+import './Users.css';
 class Users extends Component{
     render(){
         return(
-            <div>
-                <h1>Users</h1>
+            <div className= "users">
+                <table className="userTable"> 
+                    <tr>
+                        <th>UserID</th>
+                        <th>Email</th>
+                        <th>Username</th>
+                        <th>Admin</th>
+                        <th>Delete User</th>
+                    </tr>
+                    <tr>
+                        <th>12345</th>
+                        <th>gchalla@com.com</th>
+                        <th>gchalla</th>
+                        <th>True</th>
+                        <th><button className="btn"><i className="fa fa-close"></i></button></th>
+                    </tr>
+                    <tr>
+                        <th>12345</th>
+                        <th>gchalla@com.com</th>
+                        <th>gchalla</th>
+                        <th>True</th>
+                        <th><button className="btn"><i className="fa fa-close"></i></button></th>
+                    </tr>
+                </table>
             </div>
         )
     }
