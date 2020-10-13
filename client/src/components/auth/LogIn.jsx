c16374a2697846f0a8b48b8f42c5010966313269
Charan Gautam <gautamsaicharan@yahoo.com>
2020-10-12 19:09:28 -0600

    sign-in-page

--- client/src/components/auth/LogIn.jsx
+++ client/src/components/auth/LogIn.jsx
@@ -10,12 +10,15 @@ class LogIn extends Component{
             <div className="sign-in container-fluid">
                 <form action="" className="sign-in-form">
                         <h1 className="title">Sign In</h1>
-                        <p className="sign-in-p">Help students with your honest rating!</p>
-                        <label className="input-label" htmlFor="username">Username:</label><br/>
+                        <p className="sign-in-p mb-4">Help students with your honest ratings!</p>
+                        <label className="input-label" htmlFor="username">Username <span>*</span></label><br/>
                         <input className="input-ans" type="text"/>
-                        <label className="input-label" htmlFor="password">Password:</label><br/>
+                        <label className="input-label" htmlFor="password">Password <span>*</span></label><br/>
                         <input className="input-ans"type="password"/>
-                        <button className="btn btn-outline-light">Sign In</button>
+                        <button className="btn btn-outline-success btn-block">Sign In</button>
+                        <a href="" className="btn btn-block" role="button">Forgot Password?</a>
+                        <hr/>
+                        <a href="" className="sign-up-btn btn btn-block btn-outline-info" role="button">Sign Up</a>
                 </form>
             </div>
         )
