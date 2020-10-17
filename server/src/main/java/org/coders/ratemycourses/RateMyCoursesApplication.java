4bcc214f8ba4f82a820560c32990a37e5747763f
Yong <ycho97@uwo.ca>
2020-10-17 18:58:35 -0400

    login/signup user

--- server/src/main/java/org/coders/ratemycourses/RateMyCoursesApplication.java
+++ server/src/main/java/org/coders/ratemycourses/RateMyCoursesApplication.java
@@ -2,8 +2,9 @@ package org.coders.ratemycourses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class RateMyCoursesApplication {
 
 	public static void main(String[] args) {
