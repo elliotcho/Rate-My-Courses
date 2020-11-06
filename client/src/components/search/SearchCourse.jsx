e4adbe134d4ec2c254f97905d43cb26953e6ab81
Charan Gautam <gautamsaicharan@yahoo.com>
2020-11-05 21:02:25 -0700

    homePage

--- client/src/components/search/SearchCourse.jsx
+++ client/src/components/search/SearchCourse.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {getAllDepartments} from '../../store/actions/departmentActions';
 import {getCoursesInDepartment} from '../../store/actions/courseActions';
 import './css/SearchCourse.css';
+import logo from '../../images/logo.png';
 
 class SearchCourse extends Component{
     constructor(){
@@ -35,8 +36,9 @@ class SearchCourse extends Component{
 
         return(
             <div className='search-course-container'>
+                <h1 className='brand'>📚 Rate My Courses</h1>
                 <div className='search-course text-white'>
-                    <h1>Select a Department</h1>
+                    <h1 className='select-course'>Select a Department</h1>
                 
                     <select onChange={this.handleChange}>
                         <option value=''></option>
@@ -49,7 +51,7 @@ class SearchCourse extends Component{
                     </select>
                     
                     {courses.map(course => 
-                        <div key={course.id} onClick={() => this.toCourse(course.id)}>
+                        <div className='courses'key={course.id} onClick={() => this.toCourse(course.id)}>
                             {course.departmentCode} {course.number}: {course.name}
                         </div>
                     )}
