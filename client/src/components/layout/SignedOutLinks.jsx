bd73c29c33320891149b28d9cf71cc778bbb306d
Armaan Mohar <armaan.mohar@gmail.com>
2020-11-07 15:49:17 -0700

    signoutLinks working

--- client/src/components/layout/SignedOutLinks.jsx
+++ client/src/components/layout/SignedOutLinks.jsx
@@ -1,24 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
 import AuthModal from '../auth/AuthModal';
 import { Nav} from 'react-bootstrap';
 import './css/SignedOutLinks.css';
 
-class SignedOutLinks extends Component{
-    render(){
-        return(
-            <div className= "ml-auto">
-                <Nav className='signed-out-links'>
-                    <Nav.Item className='nav-item' data-toggle='modal' data-target='#auth' id='open-auth'>
-                        <i class="fas fa-address-card" title='Sign In'></i>
-                        <span>Login</span>
-                    </Nav.Item> 
-                </Nav>
+function SignedOutLinks() {
+    return(
+        <div className= "ml-auto">
+            <Nav className='signed-out-links'>
+                <Nav.Item className='nav-item' data-toggle='modal' data-target='#auth' id='open-auth'>
+                    <i class="fas fa-address-card" title='Sign In'></i>
+                    <span>Login</span>
+                </Nav.Item> 
+            </Nav>
 
-                <AuthModal/>
-            </div>
-            
-        )
-    }
+            <AuthModal/>
+        </div>
+        
+    )
 }
 
 export default SignedOutLinks;
